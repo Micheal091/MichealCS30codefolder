9150658ae5091b8e17c4401426f747fe071b4e35
Micheal091 <micheal.ikpah@rcsd.ca>
2024-09-16 08:54:21 -0600

    A1
    Ascii art

---
+++ Lessson4Demo/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("\thow as yo\nur day going");
+        System.out.println("it\'s \"sunny\" with a chance of meat balls");
+    }
+}
