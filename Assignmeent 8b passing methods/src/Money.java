bb15d74b1d2c5d53c3fb04c0f807b4aff55ec3bb
Micheal091 <micheal.ikpah@rcsd.ca>
2024-10-02 09:45:03 -0600

    INITIAL

---
+++ Assignmeent 8b passing methods/src/Money.java
@@ -0,0 +1,14 @@
+/*Program name: A 8b passing methods
+Student name: Micheal Ikpah
+Date: 10/08/2024
+
+Program Description:passing methods*/
+public class Money{
+    public void speak(){
+        System.out.println("bling bling bling");
+    }
+    public void fall(){
+        System.out.println("hahahah i escaped pssshhhh");
+    }
+
+}
---
+++ Assignmeent 8b passing methods/src/MoneyRunner.java
@@ -0,0 +1,9 @@
+public class MoneyRunner {
+    public static void main(String[] args) throws Exception {
+        Money wow = new Money();
+        wow.speak();
+        wow.fall();
+
+
+    }
+}
