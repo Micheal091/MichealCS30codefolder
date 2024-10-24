920dac79a0fb947aa7a4185c7346f8de1dacb3cc
Micheal091 <micheal.ikpah@rcsd.ca>
2024-10-24 09:27:14 -0600

    mph

---
+++ A17 miles per hour/src/MPHRunner.java
@@ -0,0 +1,37 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Micheal Ikpah
+//Date - 10/20/2024
+//Class - Comp science 30
+//Lab  - Miles per hour
+
+import java.util.Scanner; 
+import static java.lang.System.*;
+
+public class MPHRunner
+{
+	public static void main( String[] args )
+	{
+		Scanner keyboard = new Scanner(in);
+
+		out.print("Enter the distance :: ");
+		int dist = keyboard.nextInt();
+
+		out.print("Enter the hours :: ");
+		int hrs = keyboard.nextInt();
+
+		out.print("Enter the minutes :: ");
+		int mins = keyboard.nextInt();
+		keyboard.close();
+
+		MilesPerHour test = new MilesPerHour(dist, hrs, mins);
+		test.setNums(dist, hrs, mins);
+		test.calcMPH();
+		test.print();
+		
+		//add more test cases
+		
+		
+	}
+}
---
+++ A17 miles per hour/src/MilesPerHour.java
@@ -0,0 +1,45 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Micheal Ikpah
+//Date - 10/20/2024
+//Class - Comp science 30
+//Lab  - Miles per hour
+
+public class MilesPerHour
+{
+	private int distance, hours, minutes;
+	private double mph;
+
+	public MilesPerHour()
+	{
+		setNums(0,0,0);
+		mph=0.0;
+	}
+
+	public MilesPerHour(int dist, int hrs, int mins)
+	{
+
+
+	}
+
+	public void setNums(int dist, int hrs, int mins)
+	{
+		distance = dist;
+		hours = hrs;
+		minutes = mins;
+
+	}
+
+	public void calcMPH()
+	{
+		mph = (distance *60)/((hours/60)+(minutes));
+		mph = Math.round(mph);
+	}
+
+	public void print()
+	{
+System.out.println(distance+ " miles in " + hours + " hours and " + minutes + " is = " + mph + " MPH");
+
+	}
+}
